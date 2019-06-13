c6202aee3dc01cf6ca353562449c77915bd653b8
Heiko <fenghenyu@163.com>
2019-06-13 16:11:31 +0800

    仿 携程App init

---
+++ flutter_app_ctrip/android/app/src/main/java/com/heiko/flutter_app_ctrip/MainActivity.java
@@ -0,0 +1,13 @@
+package com.heiko.flutter_app_ctrip;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
