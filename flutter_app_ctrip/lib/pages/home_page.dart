53156317723a396b62a5c7dea017a4b003fb21cd
Heiko <fenghenyu@163.com>
2019-06-13 18:22:22 +0800

    轮播图Banner

--- flutter_app_ctrip/lib/pages/home_page.dart
+++ flutter_app_ctrip/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,15 +9,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    "http://pic163.nipic.com/file/20180421/7092831_140036752037_2.jpg",
+    "https://b-ssl.duitang.com/uploads/item/201507/07/20150707100822_zCAFQ.jpeg",
+    "http://img.mp.itc.cn/upload/20160428/b52be3dc215d4ecd844bf922be46f278_th.jpg",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    final PageController _controller = PageController(
-      initialPage: 0
-    );
 
     return Scaffold(
       body: Center(
-        child: Text("首页"),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 160,
+              child: Swiper(
+                itemCount: _imageUrls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.network(
+                    _imageUrls[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
